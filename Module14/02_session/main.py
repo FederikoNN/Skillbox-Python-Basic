46bbfbd960baa19242110e0dc2f2e6ad27630ef6
@kozminskii_fedor <fvk.1972@gmail.com>
2021-02-16 15:22:51 +0300

    задание 2 уравнение прямой

--- Module14/02_session/main.py
+++ Module14/02_session/main.py
@@ -1,1 +1,17 @@
-# TODO здесь писать код
+print("Введите первую точку")
+x1 = float(input('X: '))
+y1 = float(input('Y: '))
+print("\nВведите вторую точку")
+x2 = float(input('X: '))
+y2 = float(input('Y: '))
+
+print("Уравнение прямой, проходящей через эти точки:")
+
+if x2 != x1:
+    x_diff = x1 - x2
+    y_diff = y1 - y2
+    k = y_diff / x_diff
+    b = y2 - k * x2
+    print("y = ", k, " * x + ", b)
+else:
+    print("0 =", "x-", x1)
