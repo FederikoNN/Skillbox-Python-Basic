cc0760c712d0982e87d3e2290b7be3c569579409
@kozminskii_fedor <fvk.1972@gmail.com>
2021-02-16 16:28:04 +0300

    задание 3 сумма и разность

--- Module14/03_sum_and_dif/main.py
+++ Module14/03_sum_and_dif/main.py
@@ -1,1 +1,20 @@
-# TODO здесь писать код
+def sum_digit(N):
+    summ = 0
+    while N != 0:
+        summ += N % 10
+        N = N // 10
+    return summ
+
+
+def quantity_digit(N):
+    count = 0
+    while N != 0:
+        count += 1
+        N = N // 10
+    return count
+
+
+N = int(input('Введите число: '))
+print('\nСумма цифр:', sum_digit(N))
+print('Количество цифр в числе:', quantity_digit(N))
+print('Разность суммы и количества цифр:', sum_digit(N) - quantity_digit(N))
