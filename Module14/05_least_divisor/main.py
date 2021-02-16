6db4511ec71bb0cc975eda9789f33c300781c557
@kozminskii_fedor <fvk.1972@gmail.com>
2021-02-16 16:40:47 +0300

    задание 5 наименьший делитель

--- Module14/05_least_divisor/main.py
+++ Module14/05_least_divisor/main.py
@@ -1,1 +1,9 @@
-# TODO здесь писать код
+def common_divisor_min(n):
+    for x in range(2, n + 1):
+        if n % x == 0:
+            print("Наименьший делитель, отличный от единицы:", x)
+            break
+
+
+n = int(input('Введите число: '))
+common_divisor_min(n)
