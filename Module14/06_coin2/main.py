cfdaba752937fe309deea6484206dc4c864d0dfe
@kozminskii_fedor <fvk.1972@gmail.com>
2021-02-16 16:52:21 +0300

    задание 6 где монетка

--- Module14/06_coin2/main.py
+++ Module14/06_coin2/main.py
@@ -1,1 +1,15 @@
-# TODO здесь писать код
+import math
+
+
+def check(x, y, radius):
+    a = math.sqrt(x ** 2 + y ** 2)
+    if a <= radius:
+        print('\nМонетка где-то рядом')
+    else:
+        print('\nМонетки в области нет')
+
+
+x = float(input('Введите координату х: '))
+y = float(input('Введите координату y: '))
+radius = float(input('Введите радиус: '))
+check(x, y, radius)
