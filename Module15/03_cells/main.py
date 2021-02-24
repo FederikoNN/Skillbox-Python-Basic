f0cfa84ff6f54cb44a700c0279d053028ddfa26b
@kozminskii_fedor <fvk.1972@gmail.com>
2021-02-24 15:40:54 +0300

    задание 3- клетки

--- Module15/03_cells/main.py
+++ Module15/03_cells/main.py
@@ -1,1 +1,11 @@
-# TODO здесь писать код
+cells_list = [3, 0, 6, 2, 10]
+wrong_cells_list = []
+print('Кол-во клеток:', len(cells_list))
+for i in range(len(cells_list)):
+    print('Эффективность', i + 1, 'клетки:', cells_list[i])
+    if i > cells_list[i]:
+        wrong_cells_list.append(cells_list[i])
+
+print('\nНеподходящие значения:', end='')
+for cell in wrong_cells_list:
+    print(cell, end=' ')
