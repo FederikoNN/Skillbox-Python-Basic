4cbfc5106fffc36bad6e19dccf5a457a397bbcbb
@kozminskii_fedor <fvk.1972@gmail.com>
2021-02-24 16:02:39 +0300

    задание 4- видеокарты

--- Module15/04_videocards/main.py
+++ Module15/04_videocards/main.py
@@ -1,3 +1,11 @@
-# TODO здесь писать код
-
-
+video_cards_list = [3070, 2060, 3090, 3070, 3090]
+video_cards_list_new = []
+print('Кол-во видеокарт:', len(video_cards_list))
+for i in range(len(video_cards_list)):
+    print(i + 1, 'Видеокарта:', video_cards_list[i])
+    for card in video_cards_list:
+        if video_cards_list[i] < card:
+            video_cards_list_new.append(video_cards_list[i])
+            break
+print('\nСтарый список видеокарт:', video_cards_list)
+print('Новый список видеокарт:', video_cards_list_new)
