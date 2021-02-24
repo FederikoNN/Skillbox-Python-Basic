2e2966761077a67d2e60ef6d750e27839fff3656
@kozminskii_fedor <fvk.1972@gmail.com>
2021-02-24 19:43:05 +0300

    задание 8- бегущие цифры

--- Module15/08_running_nums/main.py
+++ Module15/08_running_nums/main.py
@@ -1,1 +1,7 @@
-# TODO здесь писать код
+list_old = [1, 2, 3, 4, 5]
+list_new = [0, 0, 0, 0, 0]
+K = int(input('Сдвиг: '))
+for i in range(len(list_old)):
+    list_new[(i + 1 + K) % len(list_old) - 1] = list_old[i]
+print('Изначальный список:', list_old)
+print('Сдвинутый список:', list_new)
