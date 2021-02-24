c51137e5c79aff276b7f2e9089b65408c63e46c6
@kozminskii_fedor <fvk.1972@gmail.com>
2021-02-24 20:15:23 +0300

    задание 10- сортировка

--- Module15/10_sort/main.py
+++ Module15/10_sort/main.py
@@ -1,1 +1,11 @@
-# TODO здесь писать код
+list_num = [1, 4, -3, 0, 10]
+N = len(list_num)
+tmp = 0
+print('Изначальный список:', list_num)
+for _ in range(N - 1):
+    for i in range(N - 1):
+        if list_num[i] > list_num[i + 1]:
+            tmp = list_num[i + 1]
+            list_num[i + 1] = list_num[i]
+            list_num[i] = tmp
+print('Отсортированный список:', list_num)
