59b7ccca1dfb7644452d7a28ddfbe07a1fe2f430
@kozminskii_fedor <fvk.1972@gmail.com>
2021-02-24 17:44:43 +0300

    задание 6- анализ слова

--- Module15/06_word_analysis/main.py
+++ Module15/06_word_analysis/main.py
@@ -1,1 +1,9 @@
-# TODO здесь писать код
+word = input('Введите слово: ')
+unique_letters = len(word)
+word_by_sym = list(word)
+for i in range(len(word)):
+    for j in range(i + 1, len(word)):
+        if word_by_sym[i] == word_by_sym[j]:
+            word_by_sym[j] += str(j)
+            unique_letters -= 1
+print('Кол-во уникальных букв:', unique_letters)
