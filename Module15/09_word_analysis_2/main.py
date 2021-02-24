4bdbf8c731833cff32e80f93d1068b1d4ec39973
@kozminskii_fedor <fvk.1972@gmail.com>
2021-02-24 19:56:19 +0300

    задание 9- анализ слова 2

--- Module15/09_word_analysis_2/main.py
+++ Module15/09_word_analysis_2/main.py
@@ -1,1 +1,9 @@
-# TODO здесь писать код
+word = input('Введите слово: ')
+word_sym = list(word)
+word_reverse = ''
+for i in range(len(word)):
+    word_reverse += word_sym[-(i + 1)]
+if word == word_reverse:
+    print('Слово является палиндромом')
+else:
+    print('Слово не является палиндромом')
