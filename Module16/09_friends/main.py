d4453a58b4eb5d646ccbfcaeb8edf9cb3f9378d9
@kozminskii_fedor <fvk.1972@gmail.com>
2021-03-05 01:01:48 +0300

    Задание 9- друзья

--- Module16/09_friends/main.py
+++ Module16/09_friends/main.py
@@ -1,1 +1,18 @@
-# TODO здесь писать код
+N = int(input('Кол-во друзей: '))
+K = int(input('Долговых расписок: '))
+friends_balance = []
+for _ in range(N):
+    friends_balance.append(0)
+
+for iou in range(K):
+    print()
+    print(iou + 1, 'расписка')
+    to_whom = int(input('Кому: '))
+    from_whom = int(input('От кого: '))
+    how_much = int(input('Сколько: '))
+    friends_balance[to_whom - 1] -= how_much
+    friends_balance[from_whom - 1] += how_much
+
+print('\nБаланс друзей:')
+for i_friends_balance in range(N):
+    print(i_friends_balance + 1, ':', friends_balance[i_friends_balance])
