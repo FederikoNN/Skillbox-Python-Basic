5ac85a109c4ef6d5cb6f89603db6a9a252d8f151
@kozminskii_fedor <fvk.1972@gmail.com>
2021-03-04 15:05:29 +0300

    Задание 2- шеренга

--- Module16/02_line/main.py
+++ Module16/02_line/main.py
@@ -1,1 +1,6 @@
-# TODO здесь писать код
+rank_1 = list(range(160, 177, 2))
+rank_2 = list(range(162, 181, 3))
+rank_1.extend(rank_2)
+rank_new = rank_1  # можно и не вводить новый список, а оставить rank_1
+rank_new.sort()
+print('Отсортированный список', rank_new)
