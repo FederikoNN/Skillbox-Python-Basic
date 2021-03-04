941c403cd3601730f20bcde5bf450cdee9878b5a
@kozminskii_fedor <fvk.1972@gmail.com>
2021-03-04 23:11:59 +0300

    Задание 8- считалка

--- Module16/08_rhyme_cnt/main.py
+++ Module16/08_rhyme_cnt/main.py
@@ -1,1 +1,11 @@
-# TODO здесь писать код
+N = int(input('Кол-во человек: '))
+K = int(input('Какое число в считалке? '))
+print('Значит, выбывает каждый', K, 'человек')
+people = list(range(1, N + 1))
+count_point = 0
+while len(people) > 1:
+    print('\nТекущий круг людей:', people)
+    print('Начало счёта с номера', people[count_point % len(people)])
+    count_point = (count_point + K - 1) % len(people)
+    people.remove(people[count_point])
+print('Остался человек под номером', people[0])
