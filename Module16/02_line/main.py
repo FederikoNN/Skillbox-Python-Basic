b66b98e18719eef86a02415663efd2544d24a5ff
Insolente13 <lanc05@mail.ru>
2021-03-05 08:08:29 +0300

    Module 16 02 TODO

--- Module16/02_line/main.py
+++ Module16/02_line/main.py
@@ -2,5 +2,5 @@ rank_1 = list(range(160, 177, 2))
 rank_2 = list(range(162, 181, 3))
 rank_1.extend(rank_2)
 rank_new = rank_1  # можно и не вводить новый список, а оставить rank_1
-rank_new.sort()
+rank_new.sort()  # TODO как решить без sort? =)
 print('Отсортированный список', rank_new)
