e36da81b842f32fe34d0b574ace39f8957a09b1c
@kozminskii_fedor <fvk.1972@gmail.com>
2021-03-05 10:38:12 +0300

    Задание 7- ролики (исправлено)

--- Module16/07_roller_skates/main.py
+++ Module16/07_roller_skates/main.py
@@ -11,19 +11,18 @@ def difference_min(num, array):
 count_persons = 0
 rollers = []
 rollers_quantity = int(input('Кол-во коньков: '))
-# TODO, как реализовать range таким образом, чтобы не производить в цикле вычисления (+1) с переменной цикла?
-for rollers_num in range(rollers_quantity):
-    print('Размер', rollers_num + 1, 'пары ', end='')
+
+for rollers_num in range(1, rollers_quantity + 1):
+    print('Размер', rollers_num, 'пары ', end='')
     rollers.append(int(input()))
 rollers.sort()
 
 people_num = int(input('\nКол-во людей: '))
-# TODO, как реализовать range таким образом, чтобы не производить в цикле вычисления (+1) с переменной цикла?
-for person in range(people_num):
-    print('Размер ноги', person + 1, 'человека: ', end='')
+
+for person in range(1, people_num + 1):
+    print('Размер ноги', person, 'человека: ', end='')
     foot_size = int(input())
     if foot_size != difference_min(foot_size, rollers):
         count_persons += 1
 
 print('\nНаибольшее кол-во людей, которые могут взять ролики:', count_persons)
-
