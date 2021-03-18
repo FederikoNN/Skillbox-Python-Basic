4d1f8fd2cb443d19df24eb39785ba7640d0d4eb8
@kozminskii_fedor <fvk.1972@gmail.com>
2021-03-18 12:51:14 +0300

    Задание 4- тренируемся со срезами

--- Module17/04_slices/main.py
+++ Module17/04_slices/main.py
@@ -1,3 +1,12 @@
 alphabet = 'abcdefg'
 
-# TODO здесь писать код
+print('1:', alphabet[:])
+print('\n2:', alphabet[::-1])
+print('\n3:', alphabet[::2])
+print('\n4:', alphabet[1::2])
+print('\n5:', alphabet[:1])
+print('\n6:', alphabet[:5:-1])
+print('\n7:', alphabet[3:4])
+print('\n8:', alphabet[-3:])
+print('\n9:', alphabet[3:5])
+print('\n10:', alphabet[4:2:-1])
