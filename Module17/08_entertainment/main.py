70b0157016b3cafce8d4f1a8518cb0f6e982562a
@kozminskii_fedor <fvk.1972@gmail.com>
2021-03-18 19:15:34 +0300

    Задание 8- развлечение

--- Module17/08_entertainment/main.py
+++ Module17/08_entertainment/main.py
@@ -1,1 +1,9 @@
-# TODO здесь писать код
+sticks_all = int(input('Кол-во палок: '))
+cast = int(input('\nКол-во бросков: '))
+sticks_answer = ['|' for _ in range(sticks_all)]
+for i_cast in range(1, cast + 1):
+    print('\nБросок', str(i_cast) + '.', end='')
+    L_i = int(input('Сбиты палки с номера '))
+    R_i = int(input('\nпо номер '))
+    sticks_answer[L_i - 1: R_i] = ['.' for _ in range(L_i, R_i + 1)]
+print(''.join(sticks_answer))
