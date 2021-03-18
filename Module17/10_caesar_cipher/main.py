ffaa0fa6883cc008724fbf39a1bab0a0608b11d3
@kozminskii_fedor <fvk.1972@gmail.com>
2021-03-18 23:27:42 +0300

    Задание 10- шифр Цезаря

--- Module17/10_caesar_cipher/main.py
+++ Module17/10_caesar_cipher/main.py
@@ -1,1 +1,7 @@
-# TODO здесь писать код
+alphabet = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
+text = input('Введите сообщение: ')
+shift = int(input('Введите сдвиг: '))
+
+text_crypto = [(alphabet[(alphabet.index(letter) + shift) % 33] if alphabet.count(letter) != 0 else letter) for letter
+               in text]
+print("".join(text_crypto))
