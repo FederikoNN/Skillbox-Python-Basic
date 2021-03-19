e79d696092733360c019474c45b10b1f2686f5af
Insolente13 <lanc05@mail.ru>
2021-03-19 19:31:19 +0300

    Module 17 - ok

--- Module17/05_reversal/main.py
+++ Module17/05_reversal/main.py
@@ -4,3 +4,5 @@ string_changed = string[:string.index('h')] + string[len(string) - string[::-1].
     'h') - 1:string.index('h'):-1] + string[len(string) - string[::-1].index('h') - 1:]
 
 print('\nРезультат:', string_changed)
+
+# зачёт!
