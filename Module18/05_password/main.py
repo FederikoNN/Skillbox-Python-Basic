0a920b498c1c749405970020c1437bc348705de1
@kozminskii_fedor <fvk.1972@gmail.com>
2021-03-30 09:43:34 +0300

    Задание 5- пароль

--- Module18/05_password/main.py
+++ Module18/05_password/main.py
@@ -1,1 +1,7 @@
-# TODO здесь писать код
+password = input('Придумайте пароль: ')
+
+while password.islower() or len(password) < 8 or len(
+        ''.join(digit for digit in password if digit.isdigit())) < 3:
+    password = input('Пароль ненадёжный. Попробуйте ещё раз.\nПридумайте пароль: ')
+
+print('Это надёжный пароль!')
