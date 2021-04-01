bf0bb706e3e34c1bcf28a8efa6a80c207e62357e
@kozminskii_fedor <fvk.1972@gmail.com>
2021-04-01 18:01:00 +0300

    Задание 9- сообщение

--- Module18/09_message/main.py
+++ Module18/09_message/main.py
@@ -1,1 +1,9 @@
-# TODO здесь писать код
+text_in = input('Сообщение: ')
+text_out = []
+for word in text_in.split():
+    if word.isalpha() is True:
+        text_out.append("".join(reversed(word)))
+    else:
+        text_out.append("".join(reversed(word[:-1])) + word[-1:])
+
+print('\nНовое сообщение:', " ".join(text_out))
