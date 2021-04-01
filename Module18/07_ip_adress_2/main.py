8b023a571578c6a355365ccede6e7d31be2d24cf
@kozminskii_fedor <fvk.1972@gmail.com>
2021-04-01 12:22:38 +0300

    Задание 7- IP адрес 2

--- Module18/07_ip_adress_2/main.py
+++ Module18/07_ip_adress_2/main.py
@@ -1,1 +1,19 @@
-# TODO здесь писать код
+def check_ip(ip):
+    for number in ip:
+        if number.isdigit() is False:
+            print(number + '-', 'не целое число')
+            return False
+        elif int(number) > 255:
+            print(number, 'превышает 255')
+            return False
+
+
+while True:
+    ip_address = input('Введите IP: ').split('.')
+    if len(ip_address) != 4:
+        print('Адрес - это четыре числа, разделенные точками')
+    elif check_ip(ip_address) is False:
+        continue
+    else:
+        print('IP-адрес корректен')
+
