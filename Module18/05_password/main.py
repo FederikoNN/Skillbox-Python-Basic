31a4d2e4077813d5b0b6919bf69c778bf7c18d98
@kozminskii_fedor <fvk.1972@gmail.com>
2021-04-03 12:41:46 +0300

    Задание 5 и 7- исправления по TODO

--- Module18/05_password/main.py
+++ Module18/05_password/main.py
@@ -1,13 +1,10 @@
 password = input('Придумайте пароль: ')
 
-
-
-# TODO, Не очень хорошо делать такие большие условие в условии цикла while.
-#  Предлагаю попробовать реализовать циклом while True с проверками внутри.
-#  Цель, сделать код более читаемым =)
-while password.islower() or len(password) < 8 or len(
-        ''.join(digit for digit in password if digit.isdigit())) < 3:
-    password = input('Пароль ненадёжный. Попробуйте ещё раз.\nПридумайте пароль: ')
-
-print('Это надёжный пароль!')
-
+while True:
+    if password.islower() or len(password) < 8:
+        password = input('Пароль ненадёжный. Попробуйте ещё раз.\nПридумайте пароль: ')
+    elif len(''.join(digit for digit in password if digit.isdigit())) < 3:
+        password = input('Пароль ненадёжный. Попробуйте ещё раз.\nПридумайте пароль: ')
+    else:
+        print('Это надёжный пароль!')
+        break
--- Module18/07_ip_adress_2/main.py
+++ Module18/07_ip_adress_2/main.py
@@ -2,18 +2,18 @@ def check_ip(ip):
     for number in ip:
         if number.isdigit() is False:
             print(number + '-', 'не целое число')
-            return False
+            return True
         elif int(number) > 255:
             print(number, 'превышает 255')
-            return False
+            return True
 
 
 while True:
     ip_address = input('Введите IP: ').split('.')
     if len(ip_address) != 4:
         print('Адрес - это четыре числа, разделенные точками')
-    elif check_ip(ip_address) is False:  # TODO, лучше " not check_ip(ip_address)"
-        continue  # TODO, лучше pass. Т.к. continue ничего не пропускает. Код отработает чуть быстрее
+    elif check_ip(ip_address):
+        pass
     else:
         print('IP-адрес корректен')
 
