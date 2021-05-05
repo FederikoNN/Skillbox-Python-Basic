585942d289b116f139feb985b785bde426881a36
@kozminskii_fedor <fvk.1972@gmail.com>
2021-05-05 19:27:26 +0300

    Задание 7- Пицца

--- Module19/07_pizza/main.py
+++ Module19/07_pizza/main.py
@@ -1,1 +1,16 @@
-# TODO здесь писать код
+N_orders = int(input('Введите кол-во заказов: '))
+orders_DB = dict()
+for i_order in range(1, N_orders + 1):
+    print(i_order, 'заказ: ', end='')
+    order = input().split()
+    if order[0] not in orders_DB:
+        orders_DB[order[0]] = {order[1]: order[2]}
+    elif order[1] in orders_DB.get(order[0]):
+        orders_DB.get(order[0])[order[1]] = int(orders_DB.get(order[0], {}).get(order[1])) + int(order[2])
+    else:
+        orders_DB.get(order[0])[order[1]] = order[2]
+
+for family, orders_list in sorted(orders_DB.items()):
+    print(family + ':')
+    for pizza, quantity in sorted(orders_list.items()):
+        print('\t', pizza + ':', quantity)
