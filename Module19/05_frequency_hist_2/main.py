cb4db500ba7b4d5cf92492a2a6c19d0f2cd90211
@kozminskii_fedor <fvk.1972@gmail.com>
2021-05-05 16:07:21 +0300

    Задание 5- Гистограмма частоты 2

--- Module19/05_frequency_hist_2/main.py
+++ Module19/05_frequency_hist_2/main.py
@@ -1,1 +1,27 @@
-# TODO здесь писать код
+def dict_invert(dictionary):
+    dictionary_invert = dict()
+    for i in dictionary.keys():
+        if dictionary.get(i) in dictionary_invert.keys():
+            a = list(dictionary_invert.get(dictionary.get(i)))
+            a.append(i)
+            dictionary_invert[dictionary.get(i)] = a
+        else:
+            dictionary_invert[dictionary.get(i)] = list(i)
+    return dictionary_invert
+
+
+text = input('Введите текст: ')
+hist = dict()
+for sym in text:
+    if sym in hist:
+        hist[sym] += 1
+    else:
+        hist[sym] = 1
+
+print('Оригинальный словарь частот:')
+for key, value in sorted(hist.items()):
+    print(key, ':', value)
+
+print('\nИнвертированный словарь частот:')
+for key, value in dict_invert(hist).items():
+    print(key, ':', value)
