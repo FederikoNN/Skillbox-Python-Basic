d3a7a5b606b8e753fbd182d2b9aa4d113539f057
@kozminskii_fedor <fvk.1972@gmail.com>
2021-05-06 01:02:39 +0300

    Задание 10- Снова палиндром

--- Module19/10_palindrome_again/main.py
+++ Module19/10_palindrome_again/main.py
@@ -1,1 +1,15 @@
-# TODO здесь писать код
+def palindrome_check(text):
+    stop_count = 0
+    for sym in set(text):
+        if text.count(sym) % 2 == 1:
+            if stop_count == 1:
+                print('Нельзя сделать палиндромом')
+                return False
+            stop_count += 1
+    return True
+
+
+string = ''.join(input('Введите строку: ').lower().split())
+
+if palindrome_check(string):
+    print('Можно сделать палиндромом')
