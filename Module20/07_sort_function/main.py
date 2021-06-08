cd5fd4ae706cfc9cf48a32ade571a8aa62a2be1c
@kozminskii_fedor <fvk.1972@gmail.com>
2021-06-08 19:09:44 +0300

    Задание 07- функция сортировки

--- Module20/07_sort_function/main.py
+++ Module20/07_sort_function/main.py
@@ -1,1 +1,10 @@
-# TODO здесь писать код
+def sort_tuple(tuple_ex):
+    if not ''.join(str(i) for i in tuple_ex).isdigit():
+        return tuple_ex
+    else:
+        return tuple(sorted(list(tuple_ex)))
+
+
+tuple_old = (0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 5, 'a')
+
+print(sort_tuple(tuple_old))
