3b345a8338ab0566d8973247918f899eedac9641
@kozminskii_fedor <fvk.1972@gmail.com>
2021-06-08 20:07:37 +0300

    Задание 08- контакты3

--- Module20/08_contacts_3/main.py
+++ Module20/08_contacts_3/main.py
@@ -1,1 +1,19 @@
-# TODO здесь писать код
+phone_book = {}
+
+while True:
+    choice = int(input('Добавить контакт/Найти контакт(1/0): '))
+    if choice == 1:
+        print('Добавление контакта')
+        name = input('Введите имя и фамилию: ')
+        phone = input('Введите номер телефона: ')
+        if name in phone_book.keys():
+            print('Изменена запись для', name)
+        phone_book[name] = phone
+        print(phone_book)
+    elif choice == 0:
+        print('Поиск в телефонной книге')
+        name = input('Введите фамилию: ').lower()
+        for entry in phone_book.keys():
+            if name in entry.lower():
+                print(entry, phone_book.get(entry))
+        print('Поиск завершен!')
