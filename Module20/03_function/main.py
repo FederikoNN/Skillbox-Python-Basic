20e28da002f5859e9f598533101688ffc1c46756
@kozminskii_fedor <fvk.1972@gmail.com>
2021-06-08 15:32:43 +0300

    Задание 03- функция

--- Module20/03_function/main.py
+++ Module20/03_function/main.py
@@ -1,1 +1,15 @@
-# TODO здесь писать код
+def new_tuple_function(tuple, data):
+    if tuple.count(data) == 0:
+        new_tuple = ()
+        return new_tuple
+    elif tuple.count(data) == 1:
+        return tuple[tuple.index(data):]
+    elif tuple.count(data) >= 2:
+        tmp = tuple[tuple.index(data) + 1:].index(data) + 2 + tuple.index(data)
+        return tuple[tuple.index(data):tmp]
+
+
+text = input('Введите что-нибудь: ')
+tuple_old = tuple([sym for sym in text])
+element = input('Введите элемент: ')
+print(new_tuple_function(tuple_old, element))
