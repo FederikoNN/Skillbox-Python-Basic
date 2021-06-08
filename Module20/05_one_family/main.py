5f0bb4f0bb762d7e02459be679236a415e86a961
@kozminskii_fedor <fvk.1972@gmail.com>
2021-06-08 16:01:36 +0300

    Задание 05- одна семья

--- Module20/05_one_family/main.py
+++ Module20/05_one_family/main.py
@@ -1,1 +1,11 @@
-# TODO здесь писать код
+families = {
+    'Сидоров Никита': 35,
+    'Сидорова Алина': 34,
+    'Сидоров Павел': 10
+}
+surname = input('Введите фамилию: ').lower()
+if surname.endswith('а'):
+    surname = surname[:-1]
+for name in families.keys():
+    if surname in name.lower():
+        print(name, families.get(name))
