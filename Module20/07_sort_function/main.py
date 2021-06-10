8587e5841ac9775c52917de659f7b2b43f6a53e7
@kozminskii_fedor <fvk.1972@gmail.com>
2021-06-10 16:00:06 +0300

    Задание 07,09- доработка

--- Module20/07_sort_function/main.py
+++ Module20/07_sort_function/main.py
@@ -1,12 +1,10 @@
 def sort_tuple(tuple_ex):
-    # TODO, предлагаю немного поправить решение, стоит проверять, что все числа являются целыми числами
-    #  поможет isinstance. Если в последовательности есть число с точкой, такую последовательность отсортировать не должны.
-    if not ''.join(str(i) for i in tuple_ex).isdigit():
-        return tuple_ex
-    else:
-        return tuple(sorted(list(tuple_ex)))  # list() получился лишний.
+    for i_tuple_ex in tuple_ex:
+        if not isinstance(i_tuple_ex, int):
+            return tuple_ex
+    return sorted(tuple_ex)
 
 
-tuple_old = (0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 5, 'a')
+tuple_old = (0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 5)
 
 print(sort_tuple(tuple_old))
--- Module20/09_competition_protocol/main.py
+++ Module20/09_competition_protocol/main.py
@@ -1,34 +1,18 @@
 entry_number = int(input('Сколько записей вносится в протокол? '))
-protocol = []
+protocol = {}
 for number in range(1, entry_number + 1):
     print(number, 'запись:', end=' ')
     entry = input().split()
-    # TODO, предлагаю упростить решение и решить без использования методов remove, extend, pop и index.
-    #  По итогам цикла стоит попробовать получить словарь где Ключи это имена участников, а значения списки
-    #  c [Максимальный счёт, Номер записи].
-    # TODO, можно попробовать реализовать сортировку при помощи функции lambda.
-    #  Давайте попробуем https://pythoner.name/sortdict.
-
-    if entry[1] in protocol and int(entry[0]) > int(protocol[protocol.index(entry[1]) - 1]):
-        protocol.pop(protocol.index(entry[1]) - 1)
-        protocol.remove(entry[1])
-        protocol.extend(entry)
-    elif entry[1] in protocol and int(entry[0]) <= int(protocol[protocol.index(entry[1]) - 1]):
+    if entry[1] in protocol.keys() and int(entry[0]) <= protocol.get(entry[1])[0]:
         continue
     else:
-        protocol.extend(entry)
+        protocol[entry[1]] = [int(entry[0]), number]
 
-protocol = iter(protocol)
-protocol_new = [(int(i), y) for i, y in zip(protocol, protocol)]
+list_protocol = list(protocol.items())
+list_protocol.sort(key=lambda i: i[1][1])
+list_protocol.sort(key=lambda i: i[1][0], reverse=True)
 
 print('\nИтоги соревнований:')
-
-for i in range(1, 4):
-    max_score = 0
-    max_name = ''
-    for score, name in protocol_new:
-        if score > max_score:
-            max_score = score
-            max_name = name
-    print(i, 'место.', max_name, '(' + str(max_score) + ')')
-    protocol_new.remove((max_score, max_name))
+for i_list_protocol in range(3):
+    print(i_list_protocol + 1, 'место.', list_protocol[i_list_protocol][0],
+          '(' + str(list_protocol[i_list_protocol][1][0]) + ')')
