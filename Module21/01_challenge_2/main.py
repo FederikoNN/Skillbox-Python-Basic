391149f52f626480d137630ce7165ab681787ee7
@kozminskii_fedor <fvk.1972@gmail.com>
2021-07-06 11:25:58 +0300

    Задание 1- Challenge 2

--- Module21/01_challenge_2/main.py
+++ Module21/01_challenge_2/main.py
@@ -1,1 +1,9 @@
-# TODO здесь писать код
+def print_number(num):
+    if num == 0:
+        return
+    print_number(num - 1)
+    print(num)
+
+
+number = int(input('Введите число: '))
+print_number(number)
