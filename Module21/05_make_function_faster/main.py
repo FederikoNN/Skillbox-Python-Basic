4c7b8aec7c9481560501c893d3b0d1b104383dfe
@kozminskii_fedor <fvk.1972@gmail.com>
2021-07-06 23:13:50 +0300

    Задание 5- ускоряем работу функции

--- Module21/05_make_function_faster/main.py
+++ Module21/05_make_function_faster/main.py
@@ -1,9 +1,21 @@
-def calculating_math_func(data):
-    result = 1
-    for index in range(1, data + 1):
-        result *= index
+fact_result = [1]
+
+
+def calculating_math_func(data, data_max=1):
+    print(data_max)
+    if data > data_max:
+        result = max(fact_result)
+        for index in range(data_max, data + 1):
+            result *= index
+            fact_result.append(result)
+    else:
+        result = fact_result[data]
     result /= data ** 3
     result = result ** 10
     return result
 
-# TODO оптимизировать функцию
+
+while True:
+    number = int(input('Введите число: '))
+    print('Результат:', calculating_math_func(number, len(fact_result)))
+    print(fact_result)
