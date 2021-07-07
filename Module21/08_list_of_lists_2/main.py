09716c20e134c5346a71f5e8d0b61491abfad0a7
@kozminskii_fedor <fvk.1972@gmail.com>
2021-07-07 13:31:45 +0300

    Задание 8- Список списков 2

--- Module21/08_list_of_lists_2/main.py
+++ Module21/08_list_of_lists_2/main.py
@@ -1,4 +1,18 @@
 nice_list = [1, 2, [3, 4], [[5, 6, 7], [8, 9, 10]],
              [[11, 12, 13], [14, 15], [16, 17, 18]]]
 
-# TODO здесь писать код
+
+def list_extern(list_in):
+    list_ext = []
+    for i_list in list_in:
+
+        if isinstance(i_list, list):
+            list_ext.extend(list_extern(i_list))
+        else:
+            list_ext.append(i_list)
+
+    return list_ext
+
+
+list_extern(nice_list)
+print('Ответ:', list_extern(nice_list))
