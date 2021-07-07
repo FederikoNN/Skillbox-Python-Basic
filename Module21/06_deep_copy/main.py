a99eea6a19ef89a7cd724a82974d9508951b59c4
@kozminskii_fedor <fvk.1972@gmail.com>
2021-07-07 15:36:30 +0300

    Задание 6- Глубокое копирование

--- Module21/06_deep_copy/main.py
+++ Module21/06_deep_copy/main.py
@@ -11,4 +11,20 @@ site = {
     }
 }
 
-# TODO здесь писать код
+import json
+
+
+def print_sites(struct, data_list):
+    for data in data_list:
+        struct['html']['head']['title'] = 'Куплю/продам {} недорого'.format(data)
+        struct['html']['body']['h2'] = 'У нас самая низкая цена на {}'.format(data)
+        print('Сайт для {}:'.format(data))
+        print('site =', json.dumps(struct, ensure_ascii=False, indent=4))
+
+
+sites_num = int(input('Сколько сайтов: '))
+product = []
+for _ in range(sites_num):
+    product.append(input('Введите название продукта для нового сайта: '))
+    print_sites(site, product)
+product.clear()
