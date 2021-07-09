f0913f7e522d1357990623b23adc993c10c38707
@kozminskii_fedor <fvk.1972@gmail.com>
2021-07-09 09:33:37 +0300

    Задачи 05- исправления доп.

--- Module21/05_make_function_faster/main.py
+++ Module21/05_make_function_faster/main.py
@@ -11,6 +11,24 @@ def calculating_math_func(data, fact_result={0: 1}):
     result = result ** 10
     return result
 
+# Ещё один вариант реализации:
+
+# def calculating_math_func(data, fact_result={}):
+#     if data in fact_result.keys():
+#         result = fact_result[data]
+#     else:
+#         result = 1
+#         for index in range(1, data + 1):
+#             if index in fact_result.keys():
+#                 result = fact_result[index]
+#                 continue
+#             result *= index
+#             fact_result[index] = result
+
+    # result /= data ** 3
+    # result = result ** 10
+    # return result
+
 
 while True:
     number = int(input('Введите число: '))
