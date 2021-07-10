98bc7e7a2429386eebc374b6d34de0a872b79697
@kozminskii_fedor <fvk.1972@gmail.com>
2021-07-10 12:40:47 +0300

    Задача 02- Дзен Пайтона

--- Module22/02_zen_of_python/main.py
+++ Module22/02_zen_of_python/main.py
@@ -1,1 +1,6 @@
-# TODO здесь писать код
+file = open('zen.txt', 'r')
+zen_in = file.readlines()
+print(zen_in[len(zen_in) - 1])
+for line in reversed(zen_in[:-1]):
+    print(line, end='')
+file.close()
