f8611e768b5c9a4cf355a6a1c85388e1e6389208
@kozminskii_fedor <fvk.1972@gmail.com>
2021-07-10 14:06:48 +0300

    Задача 03- Дзен Пайтона 2

--- Module22/03_zen_of_python_2/main.py
+++ Module22/03_zen_of_python_2/main.py
@@ -1,1 +1,22 @@
-# TODO здесь писать код
+words_num = 0
+sym_num = 0
+sym_min = ''
+file = open('zen.txt', 'r')
+zen_in = file.readlines()
+lines_num = len(zen_in)
+sym_min_num = len((''.join(''.join(zen_in).split())))
+print('Количество строк:', lines_num)
+for word in ''.join(zen_in).split():
+    if word[0].isalpha():
+        words_num += 1
+    for sym in word:
+        if sym.isalpha():
+            sym_num += 1
+            if (''.join(''.join(zen_in).split())).lower().count(sym.lower()) < sym_min_num:
+                sym_min_num = (''.join(''.join(zen_in).split())).count(sym.lower())
+                sym_min = sym.lower()
+print('Количество слов:', words_num)
+print('Количество букв:', sym_num)
+print('Меньше всего (' + str(sym_min_num), 'вхождения) встречается буква:', sym_min, '(без учёта регистра)')
+
+file.close()
