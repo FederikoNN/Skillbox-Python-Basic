15c489cde26aed02dfeddb233091a7433e2cb08f
@kozminskii_fedor <fvk.1972@gmail.com>
2021-07-10 12:00:36 +0300

    Задача 01- Сумма чисел 2

--- Module22/01_nums_sum_2/main.py
+++ Module22/01_nums_sum_2/main.py
@@ -1,1 +1,6 @@
-# TODO здесь писать код
+file_in = open('numbers.txt', 'r')
+data = [int(num) for num in file_in.read().split()]
+file_in.close()
+file_out = open('answer.txt', 'w')
+file_out.write(str(sum(data)))
+file_out.close()
