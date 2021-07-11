221016f2c6d16394c3dcc2a1db9c72f4ec211a73
@kozminskii_fedor <fvk.1972@gmail.com>
2021-07-11 11:54:33 +0300

    Задача 06- Паранойя

--- Module22/06_paranoia/main.py
+++ Module22/06_paranoia/main.py
@@ -1,1 +1,22 @@
-# TODO здесь писать код
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+
+
+def caesar_cipher(string, data):
+    # string = string.lower()
+    return ([(alphabet[(alphabet.index(letter) + data) % 26] if alphabet.count(letter) != 0 else letter) for letter in
+             string.lower()])
+
+
+file_in = open('text.txt', 'r')
+file_out = open('cipher_text.txt', 'w')
+shift = 1
+print('Содержимое файла text.txt:')
+for line in file_in:
+    print(line, end='')
+    file_out.write(''.join(caesar_cipher(line, shift)).capitalize())
+    shift += 1
+
+file_in.close()
+file_out.close()
+print('\n\nСодержимое файла cipher_text.txt:')
+print(open('cipher_text.txt').read())
