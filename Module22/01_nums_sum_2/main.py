f2341eb44fce05be9b6894afc833973b8134928d
@kozminskii_fedor <fvk.1972@gmail.com>
2021-07-12 23:56:35 +0300

    Задача 01- исправления

--- Module22/01_nums_sum_2/main.py
+++ Module22/01_nums_sum_2/main.py
@@ -1,6 +1,7 @@
 file_in = open('numbers.txt', 'r')
 print('\nСодержимое файла {}:'.format('numbers.txt'))
 print(file_in.read())
+file_in.seek(0)
 data = [int(num) for num in file_in.read().split()]
 file_in.close()
 file_out = open('answer.txt', 'w+')
@@ -11,14 +12,21 @@ print('Содержимое файла {}:'.format('answer.txt'))
 print(file_out.read())
 file_out.close()
 
-# Содержимое файла numbers.txt:
-#      2
-
-# 2
-#   2
-#          2
-# Содержимое файла answer.txt:
-# 0
-
-# TODO, пока что, подсчёт данных производим не совсем корректно =)
-#  Возмодно, будет проще решить задание без использования list compression.
+# Альтернативный вариант кода без list сomprehension:
+#
+# summ = 0
+# file_in = open('numbers.txt', 'r')
+# print('\nСодержимое файла {}:'.format('numbers.txt'))
+# print(file_in.read())
+# file_in.seek(0)
+# for num in file_in.read().split():
+#     if num.isdigit():
+#         summ += int(num)
+# file_in.close()
+# file_out = open('answer.txt', 'w+')
+#
+# file_out.write(f"{summ}")
+# file_out.seek(0)
+# print('Содержимое файла {}:'.format('answer.txt'))
+# print(file_out.read())
+# file_out.close()
