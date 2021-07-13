2e18080536b2c11abc8a73a7b0f4d636fe02436d
@kozminskii_fedor <fvk.1972@gmail.com>
2021-07-13 11:20:52 +0300

    Задача 01- Имена 2

--- Module23/01_names_2/main.py
+++ Module23/01_names_2/main.py
@@ -1,1 +1,18 @@
-# TODO здесь писать код
+sym_num = 0
+line_count = 0
+with open('people.txt', 'r') as file_in:
+    for line in file_in:
+        length = len(line)
+        line_count += 1
+        if line.endswith('\n'):
+            length -= 1
+        try:
+            if length < 3:
+                raise BaseException
+        except BaseException:
+            msg = 'Ошибка! Длина {} строки меньше 3 символов!'.format(line_count)
+            print(msg)
+            with open('errors.log', 'a') as file_out:
+                file_out.write(msg + '\n')
+        sym_num += length
+print('Общая сумма символов:', sym_num)
