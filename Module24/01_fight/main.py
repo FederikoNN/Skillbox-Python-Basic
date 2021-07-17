cd1f718d4c7bb3c59269a25d31bcf3cbe3e9ba4d
@kozminskii_fedor <fvk.1972@gmail.com>
2021-07-17 15:51:14 +0300

    Задачи 01- Драка

--- Module24/01_fight/main.py
+++ Module24/01_fight/main.py
@@ -1,1 +1,41 @@
-# TODO здесь писать код
+import random
+
+
+class Warrior:
+
+    def __init__(self, number, health=100):
+        self.number = number
+        self.health = health
+        self.name = f'Воин {number}'
+
+    def attack_in(self):
+        self.health -= 20
+        if self.health >= 0:
+            print(f'У Воина {self.number} осталось здоровья: {self.health}')
+        else:
+            print(f'{self.name} погиб...')
+
+    def is_defeated(self):
+        if self.health >= 0:
+            return False
+        return True
+
+
+class Fight:
+
+    def __init__(self):
+        self.warriors = [Warrior(index) for index in range(1, 3)]
+
+    def fray(self):
+        while True:
+            num = random.randint(1, 2)
+            print(f'Атаковал {self.warriors[num // 2].name}')
+            self.warriors[num % 2].attack_in()
+            if self.warriors[num % 2].is_defeated():
+                print(f'{self.warriors[num // 2].name} одержал победу!')
+                break
+            input('Нажмите любую клавишу для продолжения...')
+
+
+fight = Fight()
+fight.fray()
