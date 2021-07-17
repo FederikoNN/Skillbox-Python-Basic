c15ab6df384877aab72248e717ac357d1dba39ce
@kozminskii_fedor <fvk.1972@gmail.com>
2021-07-17 17:53:54 +0300

    Задачи 03- Окружность

--- Module24/03_circle/main.py
+++ Module24/03_circle/main.py
@@ -1,1 +1,33 @@
-# TODO здесь писать код
+import math
+
+
+class Circle:
+    def __init__(self, x=0, y=0, radius=1):
+        self.x = x
+        self.y = y
+        self.radius = radius
+        self.center = (x, y)
+
+    def square(self):
+        return math.pi * self.radius ** 2
+
+    def perimeter(self):
+        return 2 * self.radius * math.pi
+
+    def zoom_circle(self, K):
+        self.radius = K * self.radius
+
+    def intersection_check(self, x, y, radius):
+        centers_distance = math.sqrt((self.x - x) ** 2 + (self.y - y) ** 2)
+        if centers_distance < (radius + self.radius) and centers_distance < abs(radius - self.radius):
+            print('Окружности не пересекаются')
+        else:
+            print('Окружности пересекаются')
+
+
+circle = Circle()
+print(f'Окружность:\nцентр в точке {circle.center}\nрадиус: {circle.radius}')
+print(f'Площадь окружности: {circle.square()}\nПериметр окружности: {circle.perimeter()}')
+# circle.zoom_circle(3)
+# print(circle.radius)
+# circle.intersection_check(0, 2, 1)
