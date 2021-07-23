4e2397c4bf6c52e7902c567c1ca766aab4e6ff6f
@kozminskii_fedor <fvk.1972@gmail.com>
2021-07-23 15:19:10 +0300

    Задача 03- Свой словарь

--- Module25/03_my_dict/main.py
+++ Module25/03_my_dict/main.py
@@ -1,1 +1,20 @@
-# TODO здесь писать код
+class MyDict(dict):
+    def __init__(self):
+        super().__init__()
+
+    def get(self, key):
+        if key in self.keys():
+            return self[key]
+        else:
+            return 0
+
+
+families = MyDict()
+families['Сидоров Никита'] = 35
+families['Сидорова Алина'] = 34
+families['Сидоров Павел'] = 10
+print(families, type(families))
+for key in families.keys():
+    print(f'families.get({key}) -> {families.get(key)}')
+key = 'Моня Иванов'
+print(f'families.get({key}) -> {families.get(key)}')
