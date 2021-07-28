4b71119b4ee529b59616859e285d515bf3792235
@kozminskii_fedor <fvk.1972@gmail.com>
2021-07-29 00:04:29 +0300

    Задача 03- Пути файлов

--- Module26/03_files_path/main.py
+++ Module26/03_files_path/main.py
@@ -1,1 +1,16 @@
-# TODO здесь писать код
+import os.path
+
+
+def gen_files_path(folder: str, path=os.path.abspath(os.sep)) -> str:
+    for elem in os.listdir(path):
+        path_elem = os.path.join(path, elem)
+        if os.path.isdir(path_elem) and elem == folder:
+            print(f'Каталог {folder} найден.')
+            return
+        if os.path.isfile(path_elem):
+            yield path_elem
+    print(f'Каталог {folder} не найден.')
+
+
+for files_path in gen_files_path(folder='PycharmProjects', path='C:/Users/'):
+    print(files_path)
