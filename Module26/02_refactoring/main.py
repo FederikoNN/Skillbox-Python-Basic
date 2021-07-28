2f340f9ab5f3900a1d1182766353a020093af6e4
@kozminskii_fedor <fvk.1972@gmail.com>
2021-07-28 22:00:51 +0300

    Задача 02- Рефакторинг

--- Module26/02_refactoring/main.py
+++ Module26/02_refactoring/main.py
@@ -2,16 +2,18 @@ list_1 = [2, 5, 7, 10]
 list_2 = [3, 8, 4, 9]
 to_find = 56
 
-can_continue = True
-for x in list_1:
-    for y in list_2:
-        result = x * y
-        print(x, y, result)
-        if result == to_find:
-            print('Found!!!')
-            can_continue = False
-            break
-    if not can_continue:
-        break
+result = (f'\nНайдено: {x} x {y} = {to_find}' for x in list_1 for y in list_2 if x * y == to_find)
+for i in result:
+    print(i)
 
-# TODO провести рефакторинг кода
+# can_continue = True
+# for x in list_1:
+#     for y in list_2:
+#         result = x * y
+#         print(x, y, result)
+#         if result == to_find:
+#             print('Found!!!')
+#             can_continue = False
+#             break
+#     if not can_continue:
+#         break
