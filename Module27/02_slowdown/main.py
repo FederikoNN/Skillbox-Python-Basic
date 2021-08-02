b83c6b68a4a9c4a96b704cd9c0197aaf7929b43c
@kozminskii_fedor <fvk.1972@gmail.com>
2021-08-02 11:59:57 +0300

    Задача 02- Замедление кода

--- Module27/02_slowdown/main.py
+++ Module27/02_slowdown/main.py
@@ -1,1 +1,39 @@
-# TODO здесь писать код
+import functools
+import time
+from typing import Callable, Any
+
+
+def slowdown(func: Callable) -> Any:
+    top = True
+
+    @functools.wraps(func)
+    def wrapped_func(*args, **kwargs):
+        nonlocal top
+        if top:
+            top = False
+            time.sleep(10)
+            result = func(*args, **kwargs)
+        else:
+            result = func(*args, **kwargs)
+            top = True
+        return result
+
+    return wrapped_func
+
+
+@slowdown
+def test():
+    print('<Тут что-то происходит...>')
+
+
+@slowdown
+def print_number(num):
+    if num == 0:
+        return
+    print_number(num - 1)
+    print(num)
+
+
+test()
+
+print_number(3)
