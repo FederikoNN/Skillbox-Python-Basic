08d04c8a0ff03d314d86686dc846701088eb1d67
@kozminskii_fedor <fvk.1972@gmail.com>
2021-08-02 11:38:37 +0300

    Задача 01- Как дела?

--- Module27/01_how_are_you/main.py
+++ Module27/01_how_are_you/main.py
@@ -1,1 +1,39 @@
-# TODO здесь писать код
+from typing import Callable, Any
+import functools
+
+
+def how_are_you(func: Callable) -> Callable:
+    top = True
+
+    @functools.wraps(func)
+    def wrapped_func(*args, **kwargs) -> Any:
+        nonlocal top
+        if top:
+            top = False
+            input('Как дела?')
+            print('А у меня не очень! Ладно, держи свою функцию.')
+            result = func(*args, **kwargs)
+        else:
+            result = func(*args, **kwargs)
+            top = True
+        return result
+
+    return wrapped_func
+
+
+@how_are_you
+def test():
+    print('<Тут что-то происходит...>')
+
+
+@how_are_you
+def print_number(num):
+    if num == 0:
+        return
+    print_number(num - 1)
+    print(num)
+
+
+test()
+
+print_number(3)
