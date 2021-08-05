a1bc5bcc9284fdb97947448a68c92a4b578c4499
@kozminskii_fedor <fvk.1972@gmail.com>
2021-08-05 14:39:43 +0300

    Задача 01- Работа с файлом 2

--- Module28/01_work_with_file_2/main.py
+++ Module28/01_work_with_file_2/main.py
@@ -1,1 +1,29 @@
-# TODO здесь писать код
+import os
+from typing import TextIO
+
+
+class File:
+    def __init__(self, file_name: str, mode: str) -> None:
+        self.file_name = file_name
+        self.mode = mode
+
+    def __enter__(self) -> TextIO:
+        if os.path.exists(self.file_name):
+            self.file = open(self.file_name, self.mode)
+        else:
+            self.file = open(self.file_name, 'w')
+        return self.file
+
+    def __exit__(self, exc_type, exc_val, exc_tb) -> bool:
+        self.file.close()
+        return True
+
+
+with File('example.txt', 'r') as file:
+    file.write('Всем привет!\n')
+
+with File('example.txt', 'r') as file:
+    file.write('Всем привет!\n')
+
+with File('example.txt', 'a') as file:
+    file.write('Всем пока!')
