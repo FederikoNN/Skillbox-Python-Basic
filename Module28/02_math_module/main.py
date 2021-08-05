a67a7fdc9ac5d88137aad4efa2291648ba027c30
@kozminskii_fedor <fvk.1972@gmail.com>
2021-08-05 15:36:04 +0300

    Задача 02- Математический модуль

--- Module28/02_math_module/main.py
+++ Module28/02_math_module/main.py
@@ -1,1 +1,27 @@
-# TODO здесь писать код
+import math
+
+
+class MyMath:
+    PI = math.pi
+
+    @classmethod
+    def circle_len(cls, radius) -> float:
+        return 2 * cls.PI * radius
+
+    @classmethod
+    def circle_sq(cls, radius) -> float:
+        return cls.PI * radius ** 2
+
+    @classmethod
+    def cube_vol(cls, edge) -> float:
+        return edge ** 3
+
+    @classmethod
+    def sphere_sq(cls, radius) -> float:
+        return 4 * cls.PI * radius ** 2
+
+
+res_1 = MyMath.circle_len(radius=5)
+res_2 = MyMath.circle_sq(radius=6)
+print(res_1)
+print(res_2)
