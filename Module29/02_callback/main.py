b26df2eb557f07e3f79d44f907195eeaec84f96c
@kozminskii_fedor <fvk.1972@gmail.com>
2021-08-08 22:06:33 +0300

    Задача 02,05- (доработка)

--- Module29/02_callback/main.py
+++ Module29/02_callback/main.py
@@ -6,14 +6,13 @@ app = {}
 
 def callback(key: str):
     def callback_wrapped(func: Callable) -> Callable:
-
-        # TODO, функцию было бы правильней добавлять в словарь в этом месте кода
+        app[key] = func
 
         @functools.wraps(func)
         def wrapper():
-            app[key] = func
-            # TODO, стоит обязательно запустить функцию и вернуть её запуск.
-            #  Иначе, результат работы функции потеряется в декораторе, если функция что-то возвращает.
+            result = func()
+            return result
+
         return wrapper
 
     return callback_wrapped
@@ -25,13 +24,9 @@ def example():
     return 'OK'
 
 
-example()
-
 route = app.get('//')
 if route:
     response = route()
     print('Ответ:', response)
 else:
     print('Такого пути нет')
-
-
--- Module29/05_singleton/main.py
+++ Module29/05_singleton/main.py
@@ -1,22 +1,13 @@
 import functools
 
-obj_list = []
 
-
-# TODO, пожалуйста, обратите внимание, т.к. синглотон говорит о том, что объект класса будет один,
-#  Стоит решить задание без использования дополнительного списка obj_list.
-#  Если учесть, что всё в python является объектами класса, возможно будет лучше создать аргумент у функции декоратора
-#  и присвоить значение объекта класса ему. Помните, как мы создавали аргумент для подсчёта количества вызовов функций
-#  в предыдущем уроке?
-
-
-def singleton(cls):
+def singleton(cls, dict={}):
     @functools.wraps(cls)
     def wrapp(*args, **kwargs):
-        if obj_list:
-            return obj_list[0]
+        if dict:
+            return dict[cls.__name__]
         instance = cls(*args, **kwargs)
-        obj_list.append(instance)
+        dict[cls.__name__] = instance
         return instance
 
     return wrapp
