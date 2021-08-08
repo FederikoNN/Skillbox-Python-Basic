4d1f44d2357419bf5bdc5919fb1974813ee7d468
@kozminskii_fedor <fvk.1972@gmail.com>
2021-08-08 18:29:42 +0300

    Задача 05- Синглтон

--- Module29/05_singleton/main.py
+++ Module29/05_singleton/main.py
@@ -1,1 +1,29 @@
-# TODO здесь писать код
+import functools
+
+obj_list = []
+
+
+def singleton(cls):
+    @functools.wraps(cls)
+    def wrapp(*args, **kwargs):
+        if obj_list:
+            return obj_list[0]
+        instance = cls(*args, **kwargs)
+        obj_list.append(instance)
+        return instance
+
+    return wrapp
+
+
+@singleton
+class Example:
+    pass
+
+
+my_obj = Example()
+my_another_obj = Example()
+
+print(id(my_obj))
+print(id(my_another_obj))
+
+print(my_obj is my_another_obj)
