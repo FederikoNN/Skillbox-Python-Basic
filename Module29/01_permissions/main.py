46a83cd7dced5889a31f761a4ee47a8e093c8282
@kozminskii_fedor <fvk.1972@gmail.com>
2021-08-08 12:05:26 +0300

    Задача 01- Права доступа

--- Module29/01_permissions/main.py
+++ Module29/01_permissions/main.py
@@ -1,1 +1,36 @@
-# TODO здесь писать код
+import functools
+from typing import Callable
+
+user_permissions = ['admin']
+
+
+def check_permission(user: str):
+    def check_wrapped(func: Callable) -> Callable:
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            try:
+                if user in user_permissions:
+                    return func(*args, **kwargs)
+                else:
+                    raise PermissionError
+            except PermissionError:
+                print(
+                    f'{PermissionError.__name__}: У пользователя недостаточно прав, чтобы выполнить функцию {func.__name__}')
+
+        return wrapper
+
+    return check_wrapped
+
+
+@check_permission('admin')
+def delete_site():
+    print('Удаляем сайт')
+
+
+@check_permission('user_1')
+def add_comment():
+    print('Добавляем комментарий')
+
+
+delete_site()
+add_comment()
