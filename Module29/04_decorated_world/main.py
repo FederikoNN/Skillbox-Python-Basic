ffebab245439e62dd4dd0a5e89fe671d03467ac9
@kozminskii_fedor <fvk.1972@gmail.com>
2021-08-09 11:36:21 +0300

    Задача 04- Весь мир- декоратор

--- Module29/04_decorated_world/main.py
+++ Module29/04_decorated_world/main.py
@@ -1,1 +1,32 @@
-# TODO здесь писать код
+import functools
+from typing import Callable
+
+
+def decorator_with_args_for_any_decorator(decorator):
+    def decorate(*args, **kwargs):
+        # @functools.wraps(*args, **kwargs)
+        def decorated(func):
+            return decorator(func, *args, **kwargs)
+
+        return decorated
+
+    return decorate
+
+
+@decorator_with_args_for_any_decorator
+def decorated_decorator(func: Callable, *args, **kwargs):
+    @functools.wraps(func)
+    def wrap(*other_args, **other_kwargs):
+        print(f'Переданные арги и кварги в декоратор: {args} {kwargs}')
+        result = func(*other_args, **other_kwargs)
+        return result
+
+    return wrap
+
+
+@decorated_decorator(100, 'рублей', 200, 'друзей')
+def decorated_function(text: str, num: int) -> None:
+    print("Привет", text, num)
+
+
+decorated_function("Юзер", 101)
