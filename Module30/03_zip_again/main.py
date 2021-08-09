e38f193fe3c25db226e7fe18feabf7a629295487
@kozminskii_fedor <fvk.1972@gmail.com>
2021-08-09 14:13:35 +0300

    Задача 03- И снова zip

--- Module30/03_zip_again/main.py
+++ Module30/03_zip_again/main.py
@@ -1,1 +1,7 @@
-# TODO здесь писать код
+from typing import List
+
+strings: List[str] = ['a', 'b', 'c', 'd', 'e']
+numbers: List[int] = [1, 2, 3, 4, 5, 6, 7, 8]
+
+zipped = list(map(lambda sym, num: (sym, num), strings, numbers))
+print(zipped)
