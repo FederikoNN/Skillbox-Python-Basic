70a973966f4a852f096a01a1f77f375be40eb73e
@kozminskii_fedor <fvk.1972@gmail.com>
2021-08-09 15:52:03 +0300

    Задача 04- Читабельность кода

--- Module30/04_code_readability/main.py
+++ Module30/04_code_readability/main.py
@@ -1,1 +1,19 @@
-# TODO здесь писать код
+def simple_num(num):
+    for i_num in range(2, num):
+        if not num % i_num:
+            return False
+    return True
+
+
+for number in range(2, 1001):
+    if simple_num(number):
+        print(number)
+
+print([num for num in range(2, 1000) if not [i_num for i_num in range(2, num) if not num % i_num]])
+
+simple_num_list = list(
+    filter(lambda num: not [i_num for i_num in range(2, num) if not num % i_num != 0], range(2, 1000)))
+print(simple_num_list)
+
+simple_num_list = list(filter(lambda num: all(map(lambda i_num: num % i_num != 0, range(2, num))), range(2, 1000)))
+print(simple_num_list)
