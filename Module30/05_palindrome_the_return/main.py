adfea5b1d466377d59c7a9fd7df24391f9a68181
@kozminskii_fedor <fvk.1972@gmail.com>
2021-08-09 16:50:28 +0300

    Задача 05- Палиндром: возвращение

--- Module30/05_palindrome_the_return/main.py
+++ Module30/05_palindrome_the_return/main.py
@@ -1,1 +1,12 @@
-# TODO здесь писать код
+import collections
+
+
+def can_be_poly(string):
+    sym_dict = dict(collections.Counter(string))
+    if len(list(filter(lambda sym: sym_dict[sym] % 2, sym_dict))) > 1:
+        return False
+    return True
+
+
+print(can_be_poly('ababc'))
+print(can_be_poly('abbbc'))
