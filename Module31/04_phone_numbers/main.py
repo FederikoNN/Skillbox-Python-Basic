5a8356630f5851ac1de0e69ff43fcd0c52020f1d
Dmitry Sayankin <dmitry.sayankin@gmail.com>
2021-08-13 12:41:45 +0000

    Module 31

--- Module31/04_phone_numbers/main.py
+++ Module31/04_phone_numbers/main.py
@@ -1,1 +1,18 @@
-# TODO здесь писать код
+import re
+
+numbers = ['9999999999', '999999-999', '99999x9999']
+
+pattern_num = r'[8|9]{1}\d{9}$'
+
+
+def check_number(numbers_list: list, pattern: str) -> None:
+    count = 1
+    for num in numbers_list:
+        if re.match(pattern, num):
+            print(f'{count} номер: всё в порядке')
+        else:
+            print(f'{count} номер: не подходит')
+        count += 1
+
+
+check_number(numbers_list=numbers, pattern=pattern_num)
--- Module31/06_web_scrapping/main.py
+++ Module31/06_web_scrapping/main.py
@@ -1,1 +1,9 @@
-# TODO здесь писать код
+import re
+import requests
+
+html = input('Введите адрес страницы: ')
+tag = input('Какой тег будем искать? ')
+pattern = f'<{tag}.*>(.*)</{tag}>'
+html_req = requests.get(html)
+result = re.findall(pattern, html_req.text)
+print(result)
